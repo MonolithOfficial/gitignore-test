488cc4b6f84dff8195f5534ec246c4eee7bec1f6
MonolithOfficial <cotneaburjania@gmail.com>
2023-07-15 12:35:17 +0400

    feature: added doSomething function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        doSomething();
+    }
+
+    private static void doSomething(){
+        System.out.println("I DO SOMETHING");
     }
 }
